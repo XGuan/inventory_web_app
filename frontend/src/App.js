8478f28ce9bf4eb828698c3cddb1c270c7a55f6f
xguan <xing.guan.p@gmail.com>
2021-04-13 20:32:34 +0200

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+function App() {
+  return <Router>Noething</Router>
+}
+
+export default App
